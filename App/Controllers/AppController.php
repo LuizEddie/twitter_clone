eb33e95b01c5da7a1e98fd04e025f64d08eb4829
LuizEddie <contato.luiz1997@gmail.com>
2023-03-09 22:39:19 -0300

    Aula 523

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -1,21 +1,77 @@
 <?php
-    namespace App\Controllers;
 
-    use MF\Controller\Action;
-    use MF\Model\Container;
+namespace App\Controllers;
 
-    class AppController extends Action{
+use MF\Controller\Action;
+use MF\Model\Container;
 
-        public function timeline(){
+class AppController extends Action
+{
 
-            session_start();
+    public function timeline()
+    {
 
-            if(!empty($_SESSION['id']) && !empty($_SESSION['nome'])){
-                $this->render('timeline');
-            }else{
-                header("location: /?login=erro");
-            }
-        }
+        $this->validaAutenticacao();
+
+
+        $tweet = Container::getModel("Tweet");
+
+        $tweet->__set("id_usuario", $_SESSION['id']);
+
+        $tweets = $tweet->getAll();
+
+        $this->view->tweets = $tweets;
+
+        $this->render('timeline');
     }
 
-?>
+    public function tweet()
+    {
+
+        $this->validaAutenticacao();
+
+        $tweet = Container::getModel('Tweet');
+
+        $tweet->__set("tweet", $_POST['tweet']);
+        $tweet->__set("id_usuario", $_SESSION['id']);
+
+        $tweet->salvar();
+
+        header("Location: /timeline");
+    }
+
+    public function quemSeguir(){
+
+        $this->validaAutenticacao();
+
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+        print_r($pesquisarPor);
+
+        $usuarios = array();
+
+        if($pesquisarPor != ""){
+
+            $usuario = Container::getModel("Usuario");
+            $usuario->__set("nome", $pesquisarPor);
+            $usuarios = $usuario->getAll();
+        }
+
+        $this->view->usuarios = $usuarios;
+
+        $this->render("quemSeguir");
+
+    }
+
+    public function validaAutenticacao()
+    {
+
+        session_start();
+
+        if (!isset($_SESSION['id']) || $_SESSION['id'] == '' && !isset($_SESSION['nome']) || $_SESSION['nome'] == "") {
+            header("Location: /?login=erro");
+        } else {
+            return true;
+        }
+    }
+}
